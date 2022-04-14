142842f1a6d29dd424a711a895390b23ab0ff658
Joseph Hargrove <joseph.henry.hargrove@gmail.com>
2022-04-14 16:02:29 -0500

    Initialize Repo

---
+++ lib/treetoplover_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "treetoplover_palindrome/version"
+
+module TreetoploverPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
